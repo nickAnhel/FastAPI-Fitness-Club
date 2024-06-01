c75b5d060ee6e864abc14969b20151924352f03d
nickAnhel <anhimovn1@gmail.com>
2024-06-01 16:09:02 +0300

    Added validation for emails and phone numbers

--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -1,3 +1,5 @@
+from pydantic import Field
+
 from .base_schema import BaseChema
 from .membership_schema import MembershipGet
 
@@ -5,8 +7,8 @@ from .membership_schema import MembershipGet
 class UserCreate(BaseChema):
     first_name: str
     last_name: str
-    email: str
-    phone_number: str | None = None
+    email: str = Field(pattern=r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+    phone_number: str | None = Field(default=None, pattern=r"^[+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$")
 
 
 class UserGet(UserCreate):
